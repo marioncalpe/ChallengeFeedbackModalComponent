d0112cd288ff252bb00c4f197a8b624f90bfbace
Marion Calpena <37837069+marioncalpe@users.noreply.github.com>
2025-03-03 10:27:13 +0100

    design finish and close button work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatIconModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.sass'
 })
 export class AppComponent {
   title = 'ChallengeFeedbackModalComponent';
+  
+  openbydocuement(){
+    const modal = document.getElementById('pop-up');
+    if(modal != null){
+      modal.style.display = 'block';
+    }
+  }
+  closebydocuement(){
+    const modal = document.getElementById('pop-up');
+    if(modal != null){
+      modal.style.display = 'none';
+    }
+  }
 }
