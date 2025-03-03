bb034a973f5bf7d649aee5ddbd44897bb488d717
Marion Calpena <37837069+marioncalpe@users.noreply.github.com>
2025-03-03 16:17:56 +0100

    mis en place du rating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,105 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms'; // Si vous utilisez ngModel
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule],
+  imports: [RouterOutlet, MatIconModule, CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
   title = 'ChallengeFeedbackModalComponent';
-  
-  openbydocuement(){
+  //-------------------------------//
+  // function open and close popup //
+  //-------------------------------//
+  openbydocuement() {
     const modal = document.getElementById('pop-up');
-    if(modal != null){
+    if (modal != null) {
       modal.style.display = 'block';
     }
   }
-  closebydocuement(){
+  closebydocuement() {
     const modal = document.getElementById('pop-up');
-    if(modal != null){
+    if (modal != null) {
       modal.style.display = 'none';
     }
   }
+  //-----------------------//
+  // function save ratings //
+  //-----------------------//
+
+  ratings = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  selectedRating: number | undefined = undefined;
+
+  selectRating(ranting: number) {
+    this.selectedRating = ranting;
+  }
+
+  onSubmit() {
+    if (this.selectedRating !== undefined) {   
+      this.saveRating(this.selectedRating);
+      
+    }
+  }
+
+  saveRating(rating: number) {
+    this.closebydocuement();
+    console.log('Rating saved:', rating);
+    if(this.selectedRating === 1){
+      const modal = document.getElementById('1');
+      if (modal != null) {
+        modal.style.display = 'block';
+      }
+    }else if(this.selectedRating === 2){
+      const modal = document.getElementById('2');
+      if (modal != null) {
+        modal.style.display = 'block';
+      }
+    }else if(this.selectedRating === 3){
+      const modal = document.getElementById('3');
+      if (modal != null) {
+        modal.style.display = 'block';
+      }
+    }else if(this.selectedRating === 4){
+      const modal = document.getElementById('4');
+      if (modal != null) {
+        modal.style.display = 'block';
+      }
+    }else if(this.selectedRating === 5){
+      const modal = document.getElementById('5');
+      if (modal != null) {
+        modal.style.display = 'block';
+      }
+    }else if(this.selectedRating === 6){
+      const modal = document.getElementById('6');
+      if (modal != null) {
+        modal.style.display = 'block';
+      }
+    }else if(this.selectedRating === 7){
+      const modal = document.getElementById('7');
+      if (modal != null) {
+        modal.style.display = 'block';
+      }
+    }else if(this.selectedRating === 8){
+      const modal = document.getElementById('8');
+      if (modal != null) {
+        modal.style.display = 'block';
+      }
+    }else if(this.selectedRating === 9){
+      const modal = document.getElementById('9');
+      if (modal != null) {
+        modal.style.display = 'block';
+      }
+    }else{
+      const modal = document.getElementById('10');
+      if (modal != null) {
+        modal.style.display = 'block';
+      }
+    }
+  }
+
 }
